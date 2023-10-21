fb555d1c7ebbd6fab53ce9dc86162376d553a320
Michael Gauthier <mikegauthier222@hotmail.com>
2023-10-21 09:32:11 -0700

    add all files

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
