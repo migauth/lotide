05bcc6b54f4bdce578f512b1799958a5bbd892dc
Michael Gauthier <mikegauthier222@hotmail.com>
2023-10-23 10:40:41 -0700

    add assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🍀🍀🍀 ${actual} === ${expected}`);
+  } else {
+    console.log(`❗️❗️❗️ ${actual} !== ${expected}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [3, 2, 1])
+assertArraysEqual([1, 2, 3], [1, 2, 3])
+
