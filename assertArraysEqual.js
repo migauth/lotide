eb02eee6ae786f79e5d0f4e4622bdf0f4ff0c5f4
Michael Gauthier <mikegauthier222@hotmail.com>
2023-10-23 10:43:44 -0700

    edit tests in  assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -18,6 +18,5 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [3, 2, 1])
-assertArraysEqual([1, 2, 3], [1, 2, 3])
-
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // should pass
+assertArraysEqual([1, 2, 3], [1, 2]); // should fail
