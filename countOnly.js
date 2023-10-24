306772922993504f0e109fd68c121c29286d5d78
Michael Gauthier <mikegauthier222@hotmail.com>
2023-10-24 12:15:26 -0700

    add key item to top if statement

--- countOnly.js
+++ countOnly.js
@@ -9,7 +9,7 @@ const assertEqual = function (actual, expected) {
 const countOnly = function (allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {
-    if (itemsToCount) {
+    if (itemsToCount[item]) {
       if (results[item]) {
         results[item] += 1;
       } else {
@@ -25,6 +25,7 @@ const firstNames = [
   "Salima",
   "Fang",
   "Kavith",
+  "Agouhanna",
   "Jason",
   "Salima",
   "Fang",
