056796264a336fd880229eb48b3734ed80c2855e
Michael Gauthier <mikegauthier222@hotmail.com>
2023-10-24 12:10:51 -0700

    add countOnly.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,6 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`🟢🟢(*ΦωΦ*)🟢🟢 Yay! Assertion passed: ${actual} === ${expected}`);
+    console.log(`🟢🟢(*ΦωΦ*)🟢🟢-------> Yay! Assertion passed: ${actual} === ${expected}`);
   } else {
     console.log(`🔴🔴(ノಠ益ಠ)ノ彡┻━┻🔴🔴 NOPE! Assertion failed: ${actual} !== ${expected}`);
   }
--- countOnly.js
+++ countOnly.js
@@ -1,7 +1,40 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
-    console.log(`🟢🟢(*ΦωΦ*)🟢🟢 Yay! Assertion passed: ${actual} === ${expected}`);
+    console.log(`🟢🟢(*ΦωΦ*)🟢🟢-------> Yay! Assertion passed: ${actual} === ${expected}`);
   } else {
     console.log(`🔴🔴(ノಠ益ಠ)ノ彡┻━┻🔴🔴 NOPE! Assertion failed: ${actual} !== ${expected}`);
   }
 };
+
+const countOnly = function (allItems, itemsToCount) {
+  const results = {};
+  for (const item of allItems) {
+    if (itemsToCount) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
+
