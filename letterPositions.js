e8ea2a876af852596e16aa590d5f8ca5e1ac6c69
Michael Gauthier <mikegauthier222@hotmail.com>
2023-10-25 11:13:19 -0700

    add letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,41 @@
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🍀🍀(*ΦωΦ*)🍀🍀 Yay! Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❗️❗️(ノಠ益ಠ)ノ彡┻━┻ NOPE! ❗️❗️ Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const result = {};
+  for (let i = 0; i < sentence.length; i++) {
+    const letters = sentence[i];
+    if (letters !== " ") {
+      if (result[letters]) {
+        result[letters].push(i); //accounts for the duplicates
+      } else {
+        result[letters] = [i];   //sets the base number if no duplicate
+      }
+    }
+  }
+  return result;
+}
+
+console.log(letterPositions("lighthouse in the house"));
+
+assertArraysEqual(letterPositions("hello").e, [1]); //will pass
+assertArraysEqual(letterPositions("hello").e, [2]); //will fail
+assertArraysEqual(letterPositions("lighthouse in the house").e, [9, 16, 22]); // will pass
+
