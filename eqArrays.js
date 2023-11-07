43397dbd2e2733a753d4413dbf937efbe0da1f73
Michael Gauthier <mikegauthier222@hotmail.com>
2023-11-07 09:37:35 -0800

    refactor eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,3 @@
-// The assertEqual function takes two parameters and compares them. It will return an assertion passed message if the arguments are the same, and a fail message if they are not.
-
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion failed: ${actual} !== ${expected}`);
-  }
-};
-
 //The eqArray function checks if two argument arrays match. It will return an assertion passed message if the arguments are the same, and a fail message if they are not.
 
 const eqArrays = function (arr1, arr2) {
@@ -22,8 +12,4 @@ const eqArrays = function (arr1, arr2) {
   return true; //If all elements of both arrays are the same, it will pass.
 }
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) //Will be true.
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) //Will be false.
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); //Will return pass.
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); //Will return fail.
+module.exports = eqArrays;
