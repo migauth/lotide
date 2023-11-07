563f06040fea286ed7a034367c2a4c6e77fa7255
Michael Gauthier <mikegauthier222@hotmail.com>
2023-11-07 10:51:39 -0800

    refactor assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-//The eqArray function checks if two argument arrays match. It will return an assertion passed message if the arguments are the same, and a fail message if they are not.
-
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) { //Will fail if the arrays are not the same length.
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) { //Loops through the first array.
-    if (arr1[i] !== arr2[i]) { //Checks if the elements in both arrays are not the same.
-      return false; //If the elements are not the same, it will fail.
-    }
-  }
-  return true; //If all elements of both arrays are the same, it will pass.
-}
+const eqArrays = require('./eqArrays');
 
 //The assertArraysEqual function 
 
@@ -22,5 +10,5 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // should pass
-assertArraysEqual([1, 2, 3], [1, 2]); // should fail
+
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // Compairing two identical arrays. Will return pass.
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4]); // Compairing two identical arrays. Will return pass.
+assertArraysEqual([1], [1]); // Compairing two identical arrays. Will return pass.
+assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 3]); // Compairing two arrays with different values. Will return fail.
+assertArraysEqual([1, 2, 3], [1, 2]); // Compairing two arrays with different lengths. Will return fail.
+assertArraysEqual([1, 2, 3], []); // //Compairing an array with values and an empty array. //Will fail.
