93a02b4e449fa39c8a760f59f0890d634b415891
Michael Gauthier <mikegauthier222@hotmail.com>
2023-11-07 09:46:42 -0800

    fix test cases

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -2,4 +2,4 @@ const eqArrays = require('../eqArrays');
 const assertEqual = require('../assertEqual');
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); //Will return pass.
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); //Will return fail.
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); //Will return fail.
