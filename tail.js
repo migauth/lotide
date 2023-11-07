1126469d2f749200ba7092ed1653123c6853cfd8
Michael Gauthier <mikegauthier222@hotmail.com>
2023-11-07 09:01:44 -0800

    refactor tail

--- tail.js
+++ tail.js
@@ -1,24 +1,5 @@
-// The assertEqual function takes two parameters and compares them. It will return an assertion passed message if the arguments are the same, and a fail message if they are not.
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion failed: ${actual} !== ${expected}`);
-  }
-};
+//The tail function returns all but the first element of an array.
 
 const tail = arr => arr.slice(1);
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-assertEqual(words.length, 3);
-const none = [];
-tail(none);
-assertEqual(none.length, 0);
-const one = [1];
-tail(one);
-assertEqual(one.length, 1);
-
-assertEqual(tail(words).length, 2);
-assertEqual(tail(one).length, 0);
-assertEqual(tail(words)[0], "Lighthouse");
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,15 @@
+const assertEqual = require('../assertEqual');
+const tail = require('../tail')
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+assertEqual(words.length, 3);
+const none = [];
+tail(none);
+assertEqual(none.length, 0);
+const one = [1];
+tail(one);
+assertEqual(one.length, 1);
+
+assertEqual(tail(words).length, 2);
+assertEqual(tail(one).length, 0);
+assertEqual(tail(words)[0], "Lighthouse");
