c70c39d5c88be6621afb623d8500d8be615aa6f8
Michael Gauthier <mikegauthier222@hotmail.com>
2023-11-07 11:15:06 -0800

    fix middle

--- middle.js
+++ middle.js
@@ -9,7 +9,7 @@ const middle = function (arr) {
       return newArr;
     }
     if (i === mid && (copyArr.length % 2 === 0)) {
-      newArr.push(element - 1, element)
+      newArr.push(copyArr[element - 1], copyArr[element])
     } else if (i === mid) {
       newArr.push(element);
     }
