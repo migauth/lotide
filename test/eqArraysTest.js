6184108ac64c05298e101c0972a73a91078b4d41
Michael Gauthier <mikegauthier222@hotmail.com>
2023-11-07 09:51:27 -0800

    update comments

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,7 @@
 const eqArrays = require('../eqArrays');
 const assertEqual = require('../assertEqual');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); //Will return pass.
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); //Will return fail.
-assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3, 4]), true); //Will return pass.
-assertEqual(eqArrays([1, 2, 3], [1, 2]), false); //Will return fail.
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // Testing two identical arrays. Will return pass.
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); //Testing two arrays with same elements but different order. Will return fail.
+assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3, 4]), true); //Testing more identical arrays. Will return pass.
+assertEqual(eqArrays([1, 2, 3], [1, 2]), false); //Testing arrays with different lengths. Will return fail.
