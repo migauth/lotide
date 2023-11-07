bd34a86234f524026b00cf71b6d3921305378d28
Michael Gauthier <mikegauthier222@hotmail.com>
2023-11-07 08:26:56 -0800

    fix assertion to include second value

--- test/headTest.js
+++ test/headTest.js
@@ -5,6 +5,6 @@ assertEqual(head([5,6,7]), 5); //Will pass.
 assertEqual(head([6,6,7]), 5); //Will fail.
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); //Will pass.
 assertEqual(head([5]), 5) //Will pass.
-assertEqual(head([])); //Will pass.
+assertEqual(head([]), undefined); //Will pass.
 assertEqual(head(["Hello"]), "Hello"); //Will pass.
 assertEqual(head(["Hello"]), "Goodbye"); //Will fail.
