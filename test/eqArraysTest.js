2741a0af8ce8330bfb5a4a01f8eed7bcd9d9e0d4
Michael Gauthier <mikegauthier222@hotmail.com>
2023-11-07 09:44:49 -0800

    update test cases in eqArrayTest

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,9 +1,5 @@
 const eqArrays = require('../eqArrays');
 const assertEqual = require('../assertEqual');
 
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) //Will be true.
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) //Will be false.
-
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); //Will return pass.
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); //Will return fail.
